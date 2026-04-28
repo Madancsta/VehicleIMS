f2ba697e73626f61305918326e153d5d1a987d90
Pratyush Badal <pratyushbadal90@gmail.com>
2026-04-28 12:35:07 +0545

    Added Request and RequestPart Entities

---
+++ VehicleIMS.Domain/Entities/RequestPart.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehicleIMS.Domain.Entities
+{
+    public class RequestPart
+    {
+        [Required]
+        public int RequestId { get; set; }
+
+        [ForeignKey(nameof(RequestId))]
+        public Request Request { get; set; } = null!;
+
+        [Required]
+        public int PartId { get; set; }
+
+        [ForeignKey(nameof(PartId))]
+        public Part Part { get; set; } = null!;
+
+        [Required]
+        public int RequestQuantity { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string RequestDescription { get; set; } = string.Empty;
+    }
+}
--- VehicleIMS.Infrastructure/Data/AppDbContext.cs
+++ VehicleIMS.Infrastructure/Data/AppDbContext.cs
@@ -33,7 +33,30 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
         modelBuilder.Entity<VendorPart>()
             .HasKey(vp => new { vp.VendorId, vp.PartId });
 
+        // VendorUser composite key
         modelBuilder.Entity<VendorUser>()
             .HasKey(vu => new { vu.VendorId, vu.UserId });
+
+        // Request to Booking relationship
+        modelBuilder.Entity<Request>()
+            .HasOne(r => r.Booking)
+            .WithMany()
+            .HasForeignKey(r => r.BookingId);
+
+        // RequestPart composite key
+        modelBuilder.Entity<RequestPart>()
+            .HasKey(rp => new { rp.RequestId, rp.PartId });
+
+        // RequestPart relationships
+        modelBuilder.Entity<RequestPart>()
+            .HasOne(rp => rp.Request)
+            .WithMany(r => r.RequestParts)
+            .HasForeignKey(rp => rp.RequestId);
+
+        // RequestPart to Part relationship
+        modelBuilder.Entity<RequestPart>()
+            .HasOne(rp => rp.Part)
+            .WithMany()
+            .HasForeignKey(rp => rp.PartId);
     }
 }
