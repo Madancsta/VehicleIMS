8052fe6ab00032bc4553defe76e35f7f625b91f0
PrathaMaharjan <pratha.maharjan03@gmail.com>
2026-04-28 11:16:37 +0545

    added Sales entity

---
+++ VehicleIMS.Domain/Entities/Sales.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VehicleIMS.Domain.Enums;
+
+namespace VehicleIMS.Domain.Entities
+{
+    public class Sales
+    {
+        [Key]
+        public int SalesId { get; set; }
+
+        [Required]
+        public int BookingId { get; set; }
+
+        [Required]
+        public DateTime SalesDate { get; set; } = DateTime.UtcNow;
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal SalesAmount { get; set; }
+
+        [Required]
+        public int PaymentStatusId { get; set; }
+
+        // Navigation properties
+        [ForeignKey("BookingId")]
+        public Booking Booking { get; set; }
+
+        public PaymentStatus PaymentStatus { get; set; }
+        public Review Review { get; set; }
+    }
+}
